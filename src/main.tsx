57dc4455f2d9c0a8230b968224c0f56fa339c1f6
music-traning <nk175jp@gmail.com>
2026-09-15 06:41:40 +0900

    feat: add Vercel Analytics, update page title

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { I18nProvider } from './contexts/I18nContext'
+import { inject } from '@vercel/analytics'
 import './index.css'
+
+inject();
 
 createRoot(document.getElementById('app')!).render(
   <StrictMode>
